9bb2c5554d7b17a10d531fe0825f606388d08b97
MrAhsan5 <142119248+MrAhsan5@users.noreply.github.com>
2023-12-15 13:58:57 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import './App.css';
+import Call from './components/call/Call';
+import Dialer from './components/dialer/Dialer';
+import Recent from './components/recent/Recent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Dialer/>}/>
+    <Route path='/call' element={<Call/>}/>
+    <Route path='/recent' element={<Recent/>}/>
+    </Routes>
+    </BrowserRouter>
+ 
+
+    </>
   );
 }
 
---
+++ src/components/call/Call.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react'
+import style from './Call.module.css'
+import { GiSpeaker } from "react-icons/gi";
+import { IoIosVideocam, IoMdPersonAdd, IoIosKeypad } from "react-icons/io";
+import { BsMicMuteFill } from "react-icons/bs";
+import { MdCallEnd } from "react-icons/md";
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+
+
+const Call = () => {
+
+  const navigate = useNavigate();
+
+  const location = useLocation()
+  const [newData, setNewData] = useState()
+  const data = location.state
+
+
+  useEffect(() => {
+    if (data) {
+      setNewData(data)
+      const timeoutId = setTimeout(() => {
+        navigate('/recent', { state: { key: data } });
+      }, 10000);
+
+      return () => clearTimeout(timeoutId);
+    }
+
+  }, [navigate]);
+
+  return (
+    <>
+      <div className="container">
+        <div className="screen2">
+          <div className={style.heading}>calling mobile...</div>
+          <div className={style.number}>{data}</div>
+          <div className={style.actionButtons}>
+            <div className={style.section1}>
+              <div className={style.buttonDiv}><div className={style.icon}><GiSpeaker className={style.Logo} /></div>Speaker</div>
+              <div className={style.buttonDiv}><div className={style.icon}><IoIosVideocam className={style.Logo} /></div>FaceTime</div>
+              <div className={style.buttonDiv}><div className={style.icon}><BsMicMuteFill className={style.Logo} /></div>Mute</div>
+            </div>
+            <div className={style.section1}>
+              <div className={style.buttonDiv}><div className={style.icon}><IoMdPersonAdd className={style.Logo} /></div>Add</div>
+              <div className={style.buttonDiv}><div className={style.icon1}><Link to='/recent' state={newData}><MdCallEnd className={style.Logo} /></Link></div>End</div>
+              <div className={style.buttonDiv}><div className={style.icon}><Link to='/' ><IoIosKeypad className={style.Logo}/></Link></div>Keypad</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Call
+
+
+
+
+
